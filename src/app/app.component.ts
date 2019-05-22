9d76dae46597be7d5f0ea05bcbc943674b2e6a96
Thomas Kapp <thomaskpp@gmail.com>
2019-05-22 11:58:50 -0300

    cor de fundo do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { DialogMenuPage } from './../pages/dialog-menu/dialog-menu';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Platform, ModalController, NavParams } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  ngAfterViewInit(): void {
+    console.log('Nav: ', this.nav);
+    let curr = this.nav.getActive();
+    console.log('Current: ', curr);
+  }
   rootPage: any = HomePage;
+
+  @ViewChild('appNav') nav: NavController
 
   constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private modalCtrl: ModalController
   ) {
@@ -18,7 +26,6 @@ export class MyApp {
       splashScreen.hide();
     });
   }
-
   presentDialogMenuModal() {
     let dialogMenuModal = this.modalCtrl.create(DialogMenuPage);
     dialogMenuModal.present();
