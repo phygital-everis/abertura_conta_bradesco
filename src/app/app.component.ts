77baa8c9aea04981ce366291671a1b2700459c44
Cesar Kamide Martino <cesar.kamide.martino@everis.com>
2019-05-23 14:42:04 -0300

    shellpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Platform} from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
@@ -11,8 +11,7 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen
-  ) {
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       statusBar.styleDefault();
       splashScreen.hide();
