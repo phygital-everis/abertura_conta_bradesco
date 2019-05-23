41301332ea1efea5606f9abfb87d3036fc3e7544
Cesar Kamide Martino <cesar.kamide.martino@everis.com>
2019-05-23 14:32:03 -0300

    correção navegação menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
-import { DialogMenuPage } from './../pages/dialog-menu/dialog-menu';
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import {Platform, PopoverController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
-import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  ngAfterViewInit(): void {
-    let curr = this.nav.getActive();
-  }
-  rootPage: any = HomePage;
 
-  @ViewChild('appNav') nav: NavController
+  rootPage: any = HomePage;
 
   constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public popoverCtrl: PopoverController
   ) {
